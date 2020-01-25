90763919996b39a64b4821ed6a56fbe6535f5098
gujian9103 <gujian9103@zto.com>
2020-01-25 16:01:45 +0800

    feat: 启动类

--- src/main/resources/archetype-resources/__rootArtifactId__-schedule/Application.java
+++ src/main/resources/archetype-resources/__rootArtifactId__-schedule/Application.java
@@ -2,11 +2,13 @@ package ${package};
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author auto-generated
  */
 @SpringBootApplication
+@EnableScheduling
 public class Application {
 
   public static void main(String[] args) {
